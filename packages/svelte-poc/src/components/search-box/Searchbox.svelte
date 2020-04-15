2c66a957c7e8d16e6e7609a0cecb1eb23ce7e2ff
Sami Sayegh <samisayegh1@gmail.com>
2020-04-15 14:05:23 -0400

    create svelte dir
    https://coveord.atlassian.net/browse/KIT-6

---
+++ packages/svelte-poc/src/components/search-box/Searchbox.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let searchAsYouType = false;
+  let value = '';
+
+  function updateValue(e) {
+    value = e.target.value;
+    searchAsYouType && search();
+  }
+
+  function search() {
+    console.log(`your value is ${value}`)
+  }
+</script>
+
+<div>
+	<input on:change={updateValue}/>
+</div>
+
+<style>
+</style>
